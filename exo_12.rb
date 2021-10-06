b3f60e7384b87cc0594e34c691a036f73667eb5d
Naoxys <souleyk3@gmail.com>
2021-10-07 00:48:53 +0200

    Modif

--- exo_12.rb
+++ exo_12.rb
@@ -8,7 +8,7 @@ user_age_devide = user_age / 2
 user_age.times do |i|
 
     var1 = i 
-    var2 = user_age * i  
+    var2 = user_age - i  
     puts "il y a #{var1} ans, tu avais #{var2} ans"
     if var1== user_age_devide
     puts "Il y a #{var1} ans, tu avais la moitié de l'âge que tu as aujourd'hui"
