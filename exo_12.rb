1c4f5538aa55786daba2f60e753bd4dc91651c67
Naoxys <souleyk3@gmail.com>
2021-10-07 00:45:44 +0200

    Modif

--- exo_12.rb
+++ exo_12.rb
@@ -1,18 +1,16 @@
 puts "Quel est ta date de naissance"
-puts ">"
+print ">"
 
 birth_year = gets.chomp.to_i
-use_age = 2021 - birthYear 
+user_age = 2021 - birth_year 
 user_age_devide = user_age / 2 
 
 user_age.times do |i|
 
-    var1 = i + 1
-    var2 = user_age - i  
-
-    puts "il y a #{var1}ans, tu avais #{var2} ans"
-end
-
-if user_age_devide.odd?
-    puts "Il y a #{user_age_devide} ans, tu avais la moitié de l'âge que tu as aujourd'hui"
+    var1 = i 
+    var2 = user_age * i  
+    puts "il y a #{var1} ans, tu avais #{var2} ans"
+    if var1== user_age_devide
+    puts "Il y a #{var1} ans, tu avais la moitié de l'âge que tu as aujourd'hui"
+    end
 end
